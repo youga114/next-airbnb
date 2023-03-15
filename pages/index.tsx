0afa44d6b32f3bfaa45d6db53b01aa3af6e354be
kimyujun <kimyujun@the8games.com>
2023-03-15 16:18:38 +0900

    [Env] Add base settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    font-size: 21px;
+    color: gray;
+`;
 
 const index: React.FC = () => {
-    return <div>hello world</div>;
+    return <Container>hello world</Container>;
 };
 
 export default index;
