01fa344717398657e699b8eb2ce9b883dff1c63b
youga114 <youga114@gmail.com>
2023-03-28 16:45:48 +0900

    [RoomList] Add filter

--- pages/api/rooms/index.ts
+++ pages/api/rooms/index.ts
@@ -111,14 +111,62 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         } = req.query;
         try {
             const rooms = Data.room.getList();
+            const filteredRooms = rooms.filter((room) => {
+                if (
+                    latitude &&
+                    latitude !== "0" &&
+                    longitude &&
+                    longitude !== "0"
+                ) {
+                    if (
+                        !(
+                            Number(latitude) - 0.5 < room.latitude &&
+                            room.latitude < Number(latitude) + 0.05 &&
+                            Number(longitude) - 0.5 < room.longitude &&
+                            room.longitude < Number(longitude) + 0.05
+                        )
+                    ) {
+                        return false;
+                    }
+                }
+                if (checkInDate) {
+                    if (
+                        new Date(checkInDate as string) <
+                            new Date(room.startDate) ||
+                        new Date(checkInDate as string) > new Date(room.endDate)
+                    ) {
+                        return false;
+                    }
+                }
+                if (checkOutDate) {
+                    if (
+                        new Date(checkOutDate as string) <
+                            new Date(room.startDate) ||
+                        new Date(checkOutDate as string) >
+                            new Date(room.endDate)
+                    ) {
+                        return false;
+                    }
+                }
 
-            const limitedRooms = rooms.splice(
+                if (
+                    room.maximumGuestCount <
+                    Number(adultCount as string) +
+                        (Number(childrenCount as string) * 0.5 || 0)
+                ) {
+                    return false;
+                }
+
+                return true;
+            });
+
+            const limitedRooms = filteredRooms.splice(
                 0 + (Number(page) - 1) * Number(limit),
                 Number(limit)
             );
             const roomsWithHost = await Promise.all(
                 limitedRooms.map(async (room) => {
-                    const host = Data.user.find({ id: room.hostId });
+                    const host = await Data.user.find({ id: room.hostId });
                     return { ...room, host };
                 })
             );
